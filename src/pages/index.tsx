4213a92e9c2174a281fc9c7ada5e8be56372a4eb
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-07-25 19:19:49 -0300

    Inicio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Next.js + Tailwind CSS</title>
+      </Head>
       <h1>Hello world</h1>
     </>
   );
